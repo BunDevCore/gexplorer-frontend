496c4ed9184f65eb81f70707b71c3885864f6231
wiKapo <71190167+wiKapo@users.noreply.github.com>
2023-11-21 10:44:31 +0100

    adding achievement, login and leaderboard pages

--- src/pages/achievements.tsx
+++ src/pages/achievements.tsx
@@ -1,3 +1,22 @@
+// import AchievementBox from "@/components/AchievementBox";
+import {MainLayout, CenterLayout} from "@/styles/universal"
+import {AchievementItem, AchievementList, AchievementText, AchievementValue} from "@/styles/achievementBox";
+
 export default function Achievements() {
-    return <div></div>;
+    const achievements = [["AAA", 3, 5], ["BBB", 6, 10], ["GUN", 0, 100]]
+    // return <div><center>You have achieved nothing!!!</center></div>;
+    return <MainLayout>
+        <CenterLayout>
+            <AchievementList>
+                {achievements.map(achievement => <AchievementItem>
+                    <AchievementText>
+                        {achievement[0].toString()}
+                    </AchievementText>
+                    <AchievementValue>
+                        {achievement[1]}/{achievement[2]}
+                    </AchievementValue>
+                </AchievementItem>)}
+            </AchievementList>
+        </CenterLayout>
+    </MainLayout>;
 }
