e9e30307554209dc458b5d27f7d69f6b3a6b18f9
LempekPL <37885098+LempekPL@users.noreply.github.com>
2023-11-28 08:40:57 +0100

    fix light colors

--- src/theme/themes/light.ts
+++ src/theme/themes/light.ts
@@ -4,7 +4,9 @@ import DARK from "./dark";
 const light: Theme = {
     ...DARK,
     type: "light",
+    navbarBackgroundColor: "#a4a4a4",
     globalBackgroundColor: "white",
+    globalSecondaryBackgroundColor: "#c5c5c5",
 
 }
 export default light;
