3ee5c46674b718fb7f0b571f982bc722bd407c33
LempekPL <37885098+LempekPL@users.noreply.github.com>
2024-02-20 09:43:24 +0100

    navbar fix translation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,18 @@ import {
 import { Space } from "@/styles/universal";
 import Image from "next/image";
 import React from "react";
+import useTranslation from "next-translate/useTranslation";
 
 export default function Navbar() {
+    const {t} = useTranslation("navbar");
+
     return <>
         <Nav>
-            <NavLink href={"/leaderboard"}>Leaderboard</NavLink>
-            <NavLink href={"/map"}>Mapa</NavLink>
+            <NavLink href={"/achievements"}>{t("achievements")}</NavLink>
+            <NavLink href={"/map"}>{t("map")}</NavLink>
             <GexplorerLink href={"/"}>GEXPLORER</GexplorerLink>
-            <NavLink href={"/settings"}>Ustawienia</NavLink>
-            <NavLink href={"/account"}>Konto</NavLink>
+            <NavLink href={"/settings"}>{t("settings", null, {ns: "common"})}</NavLink>
+            <NavLink href={"/account"}>{t("account")}</NavLink>
         </Nav>
         <GexplorerIcon><Image src={"/favicon.png"} alt={"gexplorer"} fill={true}/></GexplorerIcon>
         <Space space="2rem" />
