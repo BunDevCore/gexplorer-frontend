c50381830ff2f4a910cdaace9cd8f27bf9809701
LempekPL <37885098+LempekPL@users.noreply.github.com>
2024-03-19 10:52:09 +0100

    start rewrite about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,50 +1,53 @@
-import {MainLayout} from "@/styles/universal";
+import {DefaultLayout, StandardBox} from "@/styles/universal";
 import useTranslation from "next-translate/useTranslation";
-import {AboutBox, AuthorBox} from "@/styles/about";
+import {AuthorBox, ImageBox, Name, Role, ProfileGrid} from "@/styles/about";
 import Image from "next/image"
 export default function About() {
     const {t} = useTranslation("about")
 
-    return <MainLayout>
-        <AboutBox>
+    return <DefaultLayout>
+        <StandardBox>
+            <ProfileGrid>
+                <ImageBox>
+                    <Image src={"/blackIcon.png"} alt={t("imageAlt")} fill={true}/>
+                </ImageBox>
+                <div>
+                    <Name>Piotr Lempkowski (Lempek)</Name>
+                    <Role>Frontend</Role>
+                </div>
+            </ProfileGrid>
+        </StandardBox>
+        <StandardBox>
+            <ProfileGrid>
+                <ImageBox>
+                    <Image src={"/s.png"} alt={t("imageAlt")} fill={true}/>
+                </ImageBox>
+                <Name>Stanisław Dramiński (Fen)</Name>
+            </ProfileGrid>
+        </StandardBox>
+        <StandardBox>
             <Image src={""} alt={t("imageAlt")}></Image>
             <AuthorBox>
-                <p>Lempek</p>
-                <p>{t("aboutLPK")}</p>
-                <p>github<br/>mail</p>
-            </AuthorBox>
-        </AboutBox>
-        <AboutBox>
-            <Image src={""} alt={t("imageAlt")}></Image>
-            <AuthorBox>
-                <p>Fen</p>
-                <p>{t("aboutFEN")}</p>
-                <p>github<br/>mail</p>
-            </AuthorBox>
-        </AboutBox>
-        <AboutBox>
-            <Image src={""} alt={t("imageAlt")}></Image>
-            <AuthorBox>
-                <p>wiKapo</p>
+                <p>Wiktor Polanowski (wiKapo)</p>
                 <p>{t("aboutWKP")}</p>
                 <p>github<br/>mail</p>
             </AuthorBox>
-        </AboutBox>
-        <AboutBox>
+        </StandardBox>
+        <StandardBox>
             <Image src={""} alt={t("imageAlt")}></Image>
             <AuthorBox>
-                <p>Szymon D</p>
+                <p>Szymon Drogosz (szdr)</p>
                 <p>{t("aboutSZD")}</p>
                 <p>github<br/>mail</p>
             </AuthorBox>
-        </AboutBox>
-        <AboutBox style={{marginBottom: "5rem"}}>
+        </StandardBox>
+        <StandardBox>
             <Image src={""} alt={t("imageAlt")}></Image>
             <AuthorBox>
-                <p>Szymon R</p>
+                <p>Szymon Roman (Qraven)</p>
                 <p>{t("aboutSZR")}</p>
                 <p>github<br/>mail</p>
             </AuthorBox>
-        </AboutBox>
-    </MainLayout>
+        </StandardBox>
+    </DefaultLayout>
 }
--- src/styles/about.ts
+++ src/styles/about.ts
@@ -1,34 +1,28 @@
 import styled from "styled-components";
-import Box from "@mui/material/Box";
 
-export const AboutBox = styled(Box)<{$disableBoxShadow?: boolean}>`
+export const ProfileGrid = styled.div`
+    display: flex;
+    gap: 1rem;
+`;
 
-  @media (min-width: 700px) {
-    width: 75%;
-    margin-left: 12.5%;
-  }
-  align-self: center;
-  border-radius: 1rem;
-  background-color: var(--global-secondary-background-color);
-  padding: 1rem;
-  margin-top: 1rem;
-  ${props => props.$disableBoxShadow && !props.$disableBoxShadow ? "" : "box-shadow: 0 .25rem .25rem rgba(0,0,0,50%);"}
-  justify-self: center;
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
+export const ImageBox = styled.div`
+    height: 6rem;
+    width: 6rem;
+    position: relative;
+    border-radius: 100%;
 
-  img {
-    background: #666;
-    width: 10rem;
-    aspect-ratio: 1;
-    border-radius: 0.5rem;
-  }
-  
-  p:first-child {
+    img {
+        object-fit: cover;
+    }
+`;
+
+export const Name = styled.p`
     font-weight: bold;
-    font-size: 36px;
-  }
+    font-size: 2rem;
+`;
+
+export const Role = styled.p`
+    font-size: 1.75rem;
 `;
 
 export const AuthorBox = styled.div`
